b81e98c60549d1061b8614ded13dde9078da925c
RobinHood P2P <stepweather@gmail.com>
2026-08-26 16:28:45 +0300

    Update audio.js

--- js/audio.js
+++ js/audio.js
@@ -1,22 +1,37 @@
+// js/audio.js
 const AudioManager = {
-    cityMusic: null,
+    cityTracks: [
+        'assets/music/city_theme1.ogg',
+        'assets/music/city_theme2.ogg',
+        'assets/music/city_theme3.ogg',
+        'assets/music/city_theme4.ogg',
+        'assets/music/city_theme5.ogg',
+    ],
+    currentTrackIndex: 0,
+    currentMusic: null,
     
     init() {
-        this.cityMusic = new Audio('assets/music/city_theme.ogg');
-        this.cityMusic.loop = true;
-        this.cityMusic.volume = 0.5;
+        this.currentTrackIndex = 0;
     },
     
     playCityTheme() {
-        if (this.cityMusic) {
-            this.cityMusic.play().catch(() => {});
-        }
+        this.stopCityTheme();
+        
+        const trackSrc = this.cityTracks[this.currentTrackIndex];
+        this.currentMusic = new Audio(trackSrc);
+        this.currentMusic.loop = true;
+        this.currentMusic.volume = 0.5;
+        this.currentMusic.play().catch(() => {});
+        
+        // Переключаем на следующий трек для следующего входа
+        this.currentTrackIndex = (this.currentTrackIndex + 1) % this.cityTracks.length;
     },
     
     stopCityTheme() {
-        if (this.cityMusic) {
-            this.cityMusic.pause();
-            this.cityMusic.currentTime = 0;
+        if (this.currentMusic) {
+            this.currentMusic.pause();
+            this.currentMusic.currentTime = 0;
+            this.currentMusic = null;
         }
     }
 };
