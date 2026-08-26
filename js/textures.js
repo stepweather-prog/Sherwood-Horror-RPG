6abf7a8b7258e7d9e7a8892f1db4ae3def7f539e
RobinHood P2P <stepweather@gmail.com>
2026-08-26 22:46:09 +0300

    Update textures.js

--- js/textures.js
+++ js/textures.js
@@ -1,15 +1,12 @@
 // js/textures.js
 const Textures = {
-    floor: [],
-    ceiling: [],
-    walls: [],
+    floor: null,
+    ceiling: null,
+    wall: null,
     oak: null,
     buildings: {},
     seamBottom: null,
     seamTop: null,
-    
-    floorCanvas: null,
-    ceilingCanvas: null,
     
     loaded: false,
     totalTextures: 0,
@@ -21,34 +18,23 @@ const Textures = {
             return;
         }
         
-        // Пол - 6 плиток
-        for (let i = 1; i <= 6; i++) {
-            this.loadImage(`assets/Sherwood_Square/floor_area_${i}.png`, (img) => {
-                this.floor.push(img);
-                this.checkLoaded(callback);
-            });
-        }
-        
-        // Потолок - 11 обычных + луна
-        for (let i = 1; i <= 11; i++) {
-            this.loadImage(`assets/Sherwood_Square/ceiling_area_${i}.png`, (img) => {
-                this.ceiling.push(img);
-                this.checkLoaded(callback);
-            });
-        }
-        
-        this.loadImage('assets/Sherwood_Square/area_ceiling_moon.png', (img) => {
-            this.ceiling.push(img);
+        // Стена
+        this.loadImage('assets/Sherwood_Square/wall_area_1.png', (img) => {
+            this.wall = img;
             this.checkLoaded(callback);
         });
         
-        // Стены - 5 плиток
-        for (let i = 1; i <= 5; i++) {
-            this.loadImage(`assets/Sherwood_Square/wall_area_${i}.png`, (img) => {
-                this.walls.push(img);
-                this.checkLoaded(callback);
-            });
-        }
+        // Пол
+        this.loadImage('assets/Sherwood_Square/floor_area_1.png', (img) => {
+            this.floor = img;
+            this.checkLoaded(callback);
+        });
+        
+        // Потолок
+        this.loadImage('assets/Sherwood_Square/area_ceiling_moon.png', (img) => {
+            this.ceiling = img;
+            this.checkLoaded(callback);
+        });
         
         // Дуб
         this.loadImage('assets/Sherwood_Square/oak_area.png', (img) => {
@@ -78,7 +64,7 @@ const Textures = {
             this.checkLoaded(callback);
         });
         
-        // Иконки зданий
+        // Иконки
         const buildingIcons = {
             'Чат': 'chat_button.png',
             'Профиль': 'player_profile.png',
@@ -100,9 +86,10 @@ const Textures = {
             'Таланты': 'ranger_skills_button.png',
             'Ежедневные': 'daily_quests.png',
             'Кошель': 'wallet.png',
+            'Кузница_icon': 'forge.png',
         };
         
-        this.totalTextures = 6 + 12 + 5 + 1 + 2 + 2 + Object.keys(buildingIcons).length;
+        this.totalTextures = 1 + 1 + 1 + 1 + 2 + 2 + Object.keys(buildingIcons).length;
         
         for (const [name, file] of Object.entries(buildingIcons)) {
             this.loadImage(`assets/icons/${file}`, (img) => {
@@ -122,43 +109,11 @@ const Textures = {
         img.src = src;
     },
     
-    createFloorCanvas() {
-        const canvas = document.createElement('canvas');
-        canvas.width = 1024 * 3;
-        canvas.height = 1024 * 2;
-        const c = canvas.getContext('2d');
-        
-        for (let i = 0; i < this.floor.length; i++) {
-            const x = (i % 3) * 1024;
-            const y = Math.floor(i / 3) * 1024;
-            c.drawImage(this.floor[i], x, y, 1024, 1024);
-        }
-        
-        return canvas;
-    },
-    
-    createCeilingCanvas() {
-        const canvas = document.createElement('canvas');
-        canvas.width = 1024 * 4;
-        canvas.height = 1024 * 3;
-        const c = canvas.getContext('2d');
-        
-        for (let i = 0; i < this.ceiling.length; i++) {
-            const x = (i % 4) * 1024;
-            const y = Math.floor(i / 4) * 1024;
-            c.drawImage(this.ceiling[i], x, y, 1024, 1024);
-        }
-        
-        return canvas;
-    },
-    
     checkLoaded(callback) {
         this.loadedTextures++;
         
         if (this.loadedTextures >= this.totalTextures) {
             this.loaded = true;
-            this.floorCanvas = this.createFloorCanvas();
-            this.ceilingCanvas = this.createCeilingCanvas();
             console.log('Все текстуры загружены!');
             if (callback) callback();
         }
